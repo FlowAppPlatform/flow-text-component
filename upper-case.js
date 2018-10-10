824289905f67751d2517659fae0dbdf608b900dd
Jerry Edebua <jeredebua@gmail.com>
2018-10-10 15:34:02 +0300

    wrote upper-case component

---
+++ upper-case.js
@@ -0,0 +1,48 @@
+const Flow = require('flow-platform-sdk');
+
+/*
+ *
+ * The class converts a string to upper case
+ *  
+ */
+
+class toUpperCase extends Flow.Component {
+  constructor() {
+    
+    super();
+    this.name = 'To upper case';
+
+    const text = new Flow.Property('Text', 'text');
+    text.required = true;
+    this.addProperty(text);
+
+    const success = new Flow.Port('Success');
+    const error = new Flow.Port('Error');
+
+    let data = new Flow.Property('Data', 'text');
+    success.addProperty(data);
+
+    data = new Flow.Property('Data', 'text');
+    error.addProperty(data);
+
+    this.addPort(success);
+    this.addPort(error);
+
+    // we perform operation here
+    this.attachTask(function() {
+      let port = this.getPort('Success');
+      try {
+        let text = this.getProperty('Text').data.toLocaleUpperCase();
+        port.getProperty('Data').data = text;
+      } catch(err) {
+        port = this.getPort('Error');
+        port.getProperty('Data').data = 'Cannot convert text to upper case';
+      }
+      port.emit();
+      this.taskComplete();
+    });
+
+  }
+}
+
+module.exports = toUpperCase;
